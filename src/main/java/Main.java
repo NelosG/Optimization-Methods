445b10939b6a0a2064e1880fdd97596d86c8fe01
NelosG <gleb.pushkarev@mail.ru>
2021-03-02 17:50:53 +0300

    Small bug fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        new Runner().addTask(new Task("SixthExpression",
+        new Runner(true).addTask(new Task("SeventhExpression",
                 x -> Math.pow(Math.log10(x - 2), 2.0) + Math.pow(Math.log10(10 - x), 2.0) - Math.pow(x, 0.2),
                 6., 9.9,8.72691,-0.846037))
                 .addForRun("Dichotomy", 1.E-15)
