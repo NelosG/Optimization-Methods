69396ea66c5b025e7ca1dbfa8f2dd7bdf509da98
NelosG <gleb.pushkarev@mail.ru>
2021-03-04 01:01:07 +0300

    Change //::TODO

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,11 @@ public class Main {
         new Runner(true).addTask(new Task("SeventhExpression",
                 x -> Math.pow(Math.log10(x - 2), 2.0) + Math.pow(Math.log10(10 - x), 2.0) - Math.pow(x, 0.2),
                 6., 9.9,8.72691,-0.846037))
-//                .addForRun("Dichotomy", 1.E-15)
-//                .addForRun("Fibonacci", 1.E-8)
-//                .addForRun("GoldenSection", 1.E-15)
-//                .addForRun("Parabolas", 1.E-8) //::TODO выдает фигню при эпс < 1.E-8
-                .addForRun("Brent", 1.E-8) //::TODO фиговая точность, нормально ток на 1.E-8
+                .addForRun("Dichotomy", 1.E-15)
+                .addForRun("Fibonacci", 1.E-8)
+                .addForRun("GoldenSection", 1.E-15)
+                .addForRun("Parabolas", 1.E-8) //::TODO выдает фигню при эпс < 1.E-8
+                .addForRun("Brent", 1.E-8) //::TODO фиговая точность для 7 выражения
                 .runTasks();
     }
 }
