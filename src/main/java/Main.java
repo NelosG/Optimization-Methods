cac1548f3334597d0769da74f8511eefdef79c17
NelosG <gleb.pushkarev@mail.ru>
2021-03-11 16:45:05 +0300

    Add manyModalFunction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,11 @@ public class Main {
                 .runTask(new Task("SeventhExpression",
                         x -> Math.pow(Math.log10(x - 2), 2.0) + Math.pow(Math.log10(10 - x), 2.0) - Math.pow(x, 0.2),
                         6., 9.9, 8.72691, -0.846037))
-                .runTask(new Task("NotUnimodal",
-                        x -> x + Math.pow(x, 2) - 6 * Math.pow(x, 3) - 3 * Math.pow(x, 4) + Math.pow(x, 5),
-                        -1., 4., 3.41488, -167.439));
+//                .runTask(new Task("NotUnimodal",
+//                        x -> x + Math.pow(x, 2) - 6 * Math.pow(x, 3) - 3 * Math.pow(x, 4) + Math.pow(x, 5),
+//                        -1., 4., 3.41488, -167.439));
+                        .runTask(new Task("NotUnimodal",
+                        x -> (x-1.4)*(x-3)*(x-5)*(x-2)*(x-3)*(x-4),
+                        1.4, 5., 4.696, -5.408));
     }
 }
-
