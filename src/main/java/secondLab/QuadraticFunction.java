24b7172617af85456be7ce267b63cd21d0d526f4
NelosG <gleb.pushkarev@mail.ru>
2021-04-24 02:27:17 +0300

    FixUp methods

--- src/main/java/secondLab/QuadraticFunction.java
+++ src/main/java/secondLab/QuadraticFunction.java
@@ -1,25 +1,22 @@
 package secondLab;
 
-import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-
-public class QuadraticFunction implements Function<RealVector, Double> {
-
-    private final RealMatrix a;
-    private final RealVector b;
-    private final double c;
-    private final int n;
+public class QuadraticFunction extends QuadrFunction {
+    protected RealMatrix a;
 
     public QuadraticFunction(RealMatrix a, RealVector b, double c) {
-        this.n = a.getRowDimension();
+        super();
+        this.n = b.getDimension();
         this.a = a;
         this.b = b;
         this.c = c;
+    }
+
+    @Override
+    public RealVector mulAtoVec(RealVector vector) {
+        return MatrixHelper.mul(a, (vector));
     }
 
     @Override
@@ -29,15 +26,13 @@ public class QuadraticFunction implements Function<RealVector, Double> {
                 + c;
     }
 
+    @Override
     public RealVector gradient(RealVector vector) {
         return MatrixHelper.mul(a, vector).add(b);
     }
 
-    public int getN() {
-        return n;
-    }
-
-    public RealMatrix getA(){
-        return a;
+    @Override
+    public double rBound() {
+        return MatrixHelper.getMaxEigenValue(a);
     }
 }
